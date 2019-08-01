9d4319927e1fba8dcb0ecaf09c0d969b9b2774ef
Camille Galladjov <ujinjinjin@outlook.com>
2019-08-01 18:55:42 +0300

    total competition positions parsing

--- program/monitoring/observer.py
+++ program/monitoring/observer.py
@@ -24,7 +24,7 @@ class Observer:
         """Observe current monitoring"""
 
         await self._logger.log_async(f'Started observing following students: {student_names}')
-        all_students: List[Student] = await self._parser.parse()
+        all_students, total_positions = await self._parser.parse()
         observable_students: List[Student] = list()
 
         os.system('cls')
@@ -41,24 +41,24 @@ class Observer:
 
         print()
         for student in observable_students:
-            print(f'{10*" "}{student.name}{(40 - len(student.name))*" "}{student.position}/92')
+            print(f'{10*" "}{student.name}{(40 - len(student.name))*" "}{student.position}/{total_positions}')
         print()
 
         # Compare positions
         for student in observable_students:
             for saved_student in saved_data:
                 if student.name == saved_student.name and student.position != saved_student.position:
-                    await self.notify(student.name, saved_student.position, student.position)
+                    await self.notify(student.name, saved_student.position, student.position, total_positions)
 
         # Save fresh data
         with open(settings.data_file, 'w', encoding=settings.encoding) as file:
             json.dump(StudentSerializer.serialize_many(observable_students), file, ensure_ascii=False, indent=2)
             await self._logger.log_async(f'Data saved to file {settings.data_file}')
 
-    async def notify(self, student_name: str, old_position: int, new_position: int):
+    async def notify(self, student_name: str, old_position: int, new_position: int, total_positions: int):
         """Notify observer of changes"""
 
-        if new_position > 92 > old_position:
+        if new_position > total_positions > old_position:
             playsound(settings.alert_lost)
             message: str = f'Attention! {student_name} has lost competition.'
         else:
--- program/monitoring/parser.py
+++ program/monitoring/parser.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 from typing import List
 
+import requests
 from requests import Response
+
 from program import settings
 from program.utils.logger import Logger
 from ..models.student import *
-import requests
-import re
 
 __all__ = ('Parser',)
 
 
 class Parser:
     def __init__(self):
-        self._regex = r"<tr id='\d+'>\n\s+<td class='num'>(\d+)<\/td>\n\s+<td class='fio'>([а-яА-Я\s-]+)<\/td>"
+        self._student_regex = r"<tr id='\d+'>\n\s+<td class='num'>(\d+)<\/td>\n\s+<td class='fio'>([а-яА-Я\s-]+)<\/td>"
+        self._total_positions_regex = r"<p>Всего мест — (\d+)"
         self._logger = Logger()
 
-    async def parse(self) -> List[Student]:
+    async def parse(self) -> (List[Student], int):
         await self._logger.log_async('Started parsing')
         rating_page: Response = requests.get(settings.rating_url)
         content: str = rating_page.content.decode(rating_page.encoding)
-        result: list = re.findall(self._regex, content)
+        result: list = re.findall(self._student_regex, content)
+        total_positions: int = int(re.findall(self._total_positions_regex, content)[0])
         student_list: List[Student] = [Student(item[1], int(item[0])) for item in result]
         await self._logger.log_async(f'Finished parsing. Parsed data: {student_list}')
 
-        return student_list
+        return student_list, total_positions
